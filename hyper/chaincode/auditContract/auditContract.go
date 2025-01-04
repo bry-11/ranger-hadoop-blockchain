e00aa34775bcb9982b78b07a1c5f2137a0a2a41c
Bryan Rivas <brivas@192.168.1.4>
2025-01-04 14:39:39 -0500

    add script and chaincode to cli

--- hyper/chaincode/auditContract/auditContract.go
+++ hyper/chaincode/auditContract/auditContract.go
@@ -27,12 +27,7 @@ type AuditStruct struct {
 // InitLedger adds a base set of assets to the ledger
 func (s *AuditContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
 	assets := []AuditStruct{
-		// {ID: "asset1", Color: "blue", Size: 5, Owner: "Tomoko", AppraisedValue: 300},
-		// {ID: "asset2", Color: "red", Size: 5, Owner: "Brad", AppraisedValue: 400},
-		// {ID: "asset3", Color: "green", Size: 10, Owner: "Jin Soo", AppraisedValue: 500},
-		// {ID: "asset4", Color: "yellow", Size: 10, Owner: "Max", AppraisedValue: 600},
-		// {ID: "asset5", Color: "black", Size: 15, Owner: "Adriana", AppraisedValue: 700},
-		// {ID: "asset6", Color: "white", Size: 15, Owner: "Michel", AppraisedValue: 800},
+		{ID: "1", User: "user123", Resource: "resourceX", Action: "read", Status: "success",Timestamp: "2024-12-21T10:00:00"},
 	}
 
 	for _, asset := range assets {
--- hyper/scripts/registerAudit.sh
+++ hyper/scripts/registerAudit.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "dentro"
+# Asignar los parámetros a variables
+userID=$1
+resource=$2
+action=$3
+result=$4
+timestamp=$5
+
+
+peer chaincode invoke \
+    -o orderer.example.com:7050 \
+    --ordererTLSHostnameOverride orderer.example.com \
+    --tls \
+    --cafile ${PWD}/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem \
+    -C mychannel \
+    -n basic \
+    --peerAddresses peer0.org1.example.com:7051 \
+    --tlsRootCertFiles ${PWD}/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt \
+    --peerAddresses peer0.org2.example.com:9051 \
+    --tlsRootCertFiles ${PWD}/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt \
+    -c '{"function":"InitLedger","Args":[]}'
