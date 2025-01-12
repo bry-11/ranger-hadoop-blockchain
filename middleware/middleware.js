cc944c2c080d6decff206ae85b6887d53861ecf5
Bryan Rivas <brivas@Bryans-MacBook-Pro-2.local>
2025-01-12 18:34:40 -0500

    fix middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -32,22 +32,31 @@ app.post('/init', (req, res) => {
 
 app.post('/audit', (req, res) => {
   const jsonString = JSON.stringify(req.body);
-  const command = `docker exec cli scripts/registerAudit.sh register '${jsonString}'`;
+  const command = `docker exec cli scripts/registerAudit.sh register "${jsonString}"`;
 
   // Ejecutar el comando
   exec(command, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error: ${error.message}`);
-      return res.status(500).send('Failed to register audit');
+      return res.status(500).json({
+        message: 'Failed to execute the command',
+        error: error.message,
+      });
     }
 
-    if (stderr) {
+    if (!stderr.includes('status:200')) {
       console.error(`Stderr: ${stderr}`);
-      return res.status(500).send('Error during audit registration');
+      return res.status(500).json({
+        message: 'Error during audit registration',
+        details: stderr.trim(),
+      });
     }
 
     console.log(`Stdout: ${stdout}`);
-    res.status(200).send('Audit registered successfully');
+    return res.status(200).json({
+      message: 'Audit registered successfully',
+      output: stdout.trim(),
+    });
   });
 });
 
@@ -57,16 +66,21 @@ app.get('/list', (req, res) => {
   exec(command, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error: ${error.message}`);
-      return res.status(500).send('Failed to list audits');
+      return res
+        .status(500)
+        .json({ message: 'Failed to list audits', details: stderr.trim() });
     }
 
     if (stderr) {
       console.error(`Stderr: ${stderr}`);
-      return res.status(500).send('Error during audit registration');
+      return res.status(500).json({
+        message: 'Error during audit registration',
+        details: stderr.trim(),
+      });
     }
 
     console.log(`Stdout: ${stdout}`);
-    res.status(200).send('Audit registered successfully');
+    return res.status(200).send('Audit registered successfully');
   });
 });
 
